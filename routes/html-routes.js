2617469d442b8b3b90e7e22c128567fc0d1b4c40
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-13 12:05:18 -0600

    Update html-routes.js
    Beginning to scrape from the USA Today website.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,15 @@
+// Dependencies
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("../models");
+
+const url = "https://www.usatoday.com";
+
 module.exports = app => {
     app.get("/", (req, res) => {
+        axios.get(`${url}/news`).then(response => {
+            var $ = cheerio.load(response.data);
+        })
         res.render("index");
     });
 }
