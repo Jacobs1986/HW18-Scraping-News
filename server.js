54c06521e8a4255d3bfda7599e74af37401562ba
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-13 10:45:24 -0600

    Update server.js
    Added handlebars

--- server.js
+++ server.js
@@ -22,3 +22,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+// Set Handlebars
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
