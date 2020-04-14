1470f8c11b33bf4c6796a13beee8bb5580dae95a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-14 10:49:31 -0600

    Update api-routes.js
    Added route to find an article by id

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,4 +5,13 @@ module.exports = app => {
     app.get("/articles", (req, res) => {
         db.Article.find({}).then(dbArticle => { res.json(dbArticle) });
     })
+
+    // find an article by id
+    app.get("/articles/:id", function(req, res) {
+        db.Article.findById(req.params.id)
+        .populate("note")
+        .then(dbArticle => {
+            res.json(dbArticle);
+        })
+    })
 }
