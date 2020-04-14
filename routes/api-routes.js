1dc57c30122983ea1bf30dbcf12d8bae94418fb3
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-14 12:06:38 -0600

    Adding styling
    Making everything look a little bit nicer.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,4 +14,21 @@ module.exports = app => {
             res.json(dbArticle);
         })
     })
+
+    // Save the note to the database
+    app.post("/articles/:id", function(req, res) {
+        // save the new note that gets posted to the Notes collection
+        db.Note.create(req.body)
+        // then find an article from the req.params.id
+        // and update it's "note" property with the _id of the new note
+        .then(dbNote => {
+          return db.Article.findOneAndUpdate(req.params.id, { $push: { note: dbNote._id} }, { new: true});
+        })
+        .then(dbArticle => {
+          res.json(dbArticle);
+        })
+        .catch(error => {
+          res.json(error);
+        });
+    });
 }
