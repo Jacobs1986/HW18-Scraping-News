473ee10a05d8524cf55b31d26867a2031dc455d3
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-14 10:48:59 -0600

    Update app.js
    Added the code to save a note to the database

--- public/app.js
+++ public/app.js
@@ -10,3 +10,35 @@ $.getJSON("/articles", data => {
         $("#article-list").append(card);
     })
 })
+
+$(document).on("click", "h4", function() {
+    $("#notes").empty();
+    var thisId = $(this).attr("id");
+    console.log(`The id for this article is ${thisId}`);
+
+    // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+})
