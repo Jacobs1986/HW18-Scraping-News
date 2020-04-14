be4baaccc53a453632faa6797562f7a7cc570139
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-14 08:55:15 -0600

    Updated routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,16 @@
 // Dependencies
 const axios = require("axios");
 const cheerio = require("cheerio");
-const db = require("../models");
+var db = require("../models");
 
 const url = "https://www.usatoday.com";
 
 module.exports = app => {
     app.get("/", (req, res) => {
+        res.render("index");
+    });
+
+    app.get("/articles", (req, res) => {
         axios.get(`${url}/news`).then(response => {
             var $ = cheerio.load(response.data);
             $("div[aria-label='More Top Stories'] a").each((i, element) => {
@@ -16,8 +20,15 @@ module.exports = app => {
                 result.title = $(element).text();
                 result.link = `${url}/${link}`;
                 result.summary = $(element).attr("data-c-br");
-            })
-        })
-        res.render("index");
+
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        console.log(dbArticle);
+                    }).catch(function (error) {
+                        console.log(error)
+                    });
+            });
+            res.send("Scrape Complete");
+        });
     });
 }
