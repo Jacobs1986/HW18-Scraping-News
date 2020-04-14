6fc385e3016d40cafe9a2e552d5b8cb49c841d07
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-14 10:22:10 -0600

    Update app.js
    Wrote the code that will generate the current articles in the database.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
 console.log("The file is linked up!")
+
+$.getJSON("/articles", data => {
+    console.log(data);
+    data.forEach(article => {
+        let card = $("<div>").addClass("row");
+        card.append(`<h4 id='${article._id}'>${article.title}</h4>`);
+        card.append(`<p>${article.summary}</p>`);
+        card.append(`<a href='${article.link}'>Click here for full article</a>`)
+        $("#article-list").append(card);
+    })
+})
